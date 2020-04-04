e80b317d5b744d6853959236801c892c71575483
Djo <izm@zixn.ru>
2020-04-04 13:23:31 +0300

    Релиз

---
+++ Client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentFTP;
+using System.IO;
+
+namespace ftpNodAntivirus
+{
+    class Client
+    {
+
+        /**
+         *Настройки FTP подключения
+         */
+        private string ftpHost = "";
+        private int ftpPort = 21;
+        private string ftpUser = "";
+        private string ftpPwd = "";
+
+        /**
+         * Папка с файлами которые нужно передать на сервер FTP
+         */
+        private string sourceFolder = "D:\\ob\\nod_upd\\";
+
+        public void Upload()
+        {
+            FtpClient ftp = new FtpClient(ftpHost, ftpPort, ftpUser, ftpPwd);
+
+            ftp.Connect();
+
+            /**
+             * Удаляем все файлы на FTP
+             */
+            foreach (FtpListItem item in ftp.GetListing())
+            {
+                if (item.Type == FtpFileSystemObjectType.File)
+                {
+                    Console.WriteLine("Удаляю: " + item.FullName);
+
+                    ftp.DeleteFile(item.FullName);
+                }
+            }
+
+            if (!Directory.Exists(sourceFolder))
+            {
+                return;
+            }
+
+            /**
+             * Обход локального каталога и загрузка файлов на FTP
+             */
+            foreach (string file in Directory.GetFiles(sourceFolder))
+            {
+                Console.WriteLine("Отправляю: " + file);
+
+                string fileName = Path.GetFileName(file);
+
+                ftp.UploadFile(file, $"/{fileName}");
+            }
+
+            ftp.Disconnect();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ftpNodAntivirus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Client ftpClient = new Client();
+
+            ftpClient.Upload();
+        }
+    }
+}
